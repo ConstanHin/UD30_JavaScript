3d4cb5e362b4d60fe97af3ee425709221b38cbef
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-29 18:30:04 +0200

    finalizado ejercicio 10

---
+++ js/palindromo.js
@@ -0,0 +1,30 @@
+/**
+ * Compruba si la cadena de texto recibida se lee del mismo modo al revés
+ */
+function comprobarPalindromo(text) {
+
+    // Poner en minuscula y eliminar los espacios del texto
+    text = text.toLowerCase();
+    text = text.replaceAll(" ", "");
+
+    let normalText = text;
+
+    // Convertir en array y separar por letra para dar la vuelta al string
+    let normalArr = text.split('');
+    let revesArr = normalArr;
+    revesArr = revesArr.reverse();
+    let revesText = revesArr.join('');
+
+    // Comprobar si se escriben igual
+    if (normalText === revesText) {
+        alert("Es un palindromo")
+    } else {
+        alert("No es un palindromo")
+    }
+}
+
+/*******************/
+// Obtener el input del usuario
+let text = prompt("Introducir un texto");
+//Ejecutar la función de comprobar
+comprobarPalindromo(text);
