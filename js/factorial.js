61c2e51f184f223d660746d30ef2bcf57cebde71
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-05-29 17:17:32 +0200

    finalizado ejercicio 7

---
+++ js/factorial.js
@@ -0,0 +1,17 @@
+let numeroIntroducido = prompt("Introduce el numero para calcular el factorial");
+
+if (numeroIntroducido <= 0 || isNaN(numeroIntroducido)) {
+    alert("Numero introducido incorrecto")
+} else {
+
+    for (let i = numeroIntroducido-1; i > 0; i--) {
+        numeroIntroducido = numeroIntroducido * i;   
+    }
+
+    let resultado = numeroIntroducido;
+
+    alert("El resultado es " + resultado);
+    console.log("El resultado es " + resultado);
+
+}
+
